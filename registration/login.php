dc91f807018e04a8045e62e70d9104af7a04be64
Minthu1996 <60377621+Minthu1996@users.noreply.github.com>
2020-05-22 11:21:25 +0630

    registration form with php and mysql

---
+++ registration/login.php
@@ -0,0 +1,37 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <title>User registration system using PHP and MySQL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+     <h2>login</h2>
+
+</div>
+   
+   <form method="post" action="login.php">
+   <?php include('errors.php'); ?>
+    <div class="input-group">
+       <label>Username</label>
+       <input type="text" name="username">
+    </div>
+   
+    <div class="input-group">
+       <label>Password</label>
+       <input type="password" name="password">
+    </div>
+   
+      <div class="input-group">
+         <button type="submit" name="login" class="btn">Login</button>
+      </div>
+      <p>
+          Not yet a member? <a href="register.php">Sign in</a>
+      </p>
+    
+   </form>
+    
+</body>
+</html>
